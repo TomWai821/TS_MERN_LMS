a2790231174e799e1ddedada8a3c4b5474358220
TomWai821 <waichueng821@gmail.com>
2026-02-25 12:52:08 +0800

    backend:Practice the improvement part in ./backend/src/improvement/*

---
+++ backend/src/Improvement/SuccessResponse.ts
@@ -0,0 +1,26 @@
+type successCode = '0000' | '0001' | '0002';
+
+const successCodeList: Record<successCode, string> =
+{
+    '0000': "Authentication successful!",
+    '0001': "No error, The request was successful",
+    '0002': "No error, but there is no data to return"
+}
+
+export const successResponse = (successCode: successCode, message: string, data?: [any], totalRecord?: number) => 
+{
+    if(successCode === '0000')
+    {
+        console.log(`Invalid success code: ${successCode} - ${message}`);
+        return;
+    }
+
+    const responseData = { totalRecord: totalRecord !== undefined ? totalRecord : (data ? (Array.isArray(data) ? data.length : 1) : 0), data }
+
+    return { success: true, errorCode: successCode, errorMessage: successCodeList[successCode], message, ...responseData };
+}
+
+export const successAuthenicationResponse = (message: string) => 
+{
+    return { success: true, errorCode: '0000', errorMessage: successCodeList['0000'], message };
+}
