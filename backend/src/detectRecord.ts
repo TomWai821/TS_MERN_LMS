bcd4a1a82eb4af33433a3ed7c6e6a393db61335b
TomWai821 <waichueng821@gmail.com>
2026-03-02 16:32:56 +0800

    doc: Put the GetDataFromGoogleBookAPI into doc/image/Diagrams

--- backend/src/detectRecord.ts
+++ backend/src/detectRecord.ts
@@ -1,10 +1,32 @@
 import { detectExpiredLoanRecord, modifyFinesAmount } from "./schema/book/bookLoaned";
 import { detectExpiredSuspendRecord } from "./schema/user/suspendList";
 
-export const detectRecordsDaily = () => 
+const taskList: (() => void)[] =
+[
+    detectExpiredSuspendRecord,
+    modifyFinesAmount,
+    detectExpiredLoanRecord
+]
+
+export const scheduleDailyMidnightTasks = () => 
 {
-    const DayToMillionSeconds = 24 * 60 * 60 * 1000;
-    setInterval(detectExpiredSuspendRecord, DayToMillionSeconds);
-    setInterval(modifyFinesAmount, DayToMillionSeconds);
-    setInterval(detectExpiredLoanRecord, DayToMillionSeconds);
-}
+    const now = new Date();
+
+    const UTCYear = now.getUTCFullYear();
+    const UTCMonth = now.getUTCMonth();
+    const getDate = now.getUTCDate();
+
+    const nextMidnight_UTC8 = new Date(UTCYear, UTCMonth, getDate + 1, 0, 0, 0, 0);
+    nextMidnight_UTC8.setHours(nextMidnight_UTC8.getUTCHours() + 8);
+
+    const delay = nextMidnight_UTC8.getTime() - now.getTime();
+
+    setTimeout(() => 
+    {
+        // Implement each task
+        taskList.forEach(task => task());
+
+        // Run at fixed interval (every 24h)
+        setInterval(() => { taskList.forEach(task => task()) }, 24 * 60 * 60 * 1000);
+    }, delay);
+};
