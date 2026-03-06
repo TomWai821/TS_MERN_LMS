7edf2c5e29d62cd1e4a520819411f991e01641df
TomWai821 <waichueng821@gmail.com>
2026-03-06 19:59:17 +0800

    backend: modify jest.config.ts and jtest.setup.ts for backend test environment

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = 
 {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testMatch: ["**/?(*.)+(spec|test).[tj]s"],
-    setupFiles: ["<rootDir>/jest.setup.ts"]
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).[tj]s"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
